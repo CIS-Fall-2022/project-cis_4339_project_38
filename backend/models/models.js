7c4a32f65f498be4b4ef19a52b83ee70e882c433
mtzaidi2 <89554587+mtzaidi2@users.noreply.github.com>
2022-09-14 21:22:52 -0500

    Add files via upload
    Updated schema data attributes, adding expansion for multiple orgs, linked schemas via ref

--- backend/models/models.js
+++ backend/models/models.js
@@ -3,8 +3,49 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //collection for intakeData
-let primaryDataSchema = new Schema({
-    _id: { type: String, default: uuid.v1 },
+//client data is for guests of the nonprofit orgs
+let org_data = new Schema({
+	org_id: { type: String, default: uuid.v1 },
+	org_name: {
+		type: String,
+		require = true
+	},
+	org_description: {
+		type: String,
+	}
+	org_contact: {
+		phoneNumbers: {
+        	type: Array,
+        	require: true
+   		},
+		email: {
+        	type: String,
+		}
+    },
+	org_address: {
+        line1: {
+            type: String,
+        },
+        line2: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: String,
+        }
+    },
+}, {
+    collection: 'organizationData',
+    timestamps: true
+
+});
+let client_data = new Schema({
+    client_id: { type: String, default: uuid.v1 },
     firstName: {
         type: String,
         require: true
@@ -14,25 +55,36 @@ let primaryDataSchema = new Schema({
     },
     lastName: {
         type: String,
-        required: true
+        require: true
     },
-    email: {
-        type: String
+    contact: {
+		phoneNumbers: {
+        	type: Array,
+        	require: true
+   		},
+		email: {
+        	type: String,
+		}
     },
-    phoneNumbers: {
-        type: Array,
-        required: true
+	date_birth: {
+		type: Date,
+	},
+	gender: {
+        type: String,
     },
+	ethnicity: {
+		type: String,
+	},
     address: {
         line1: {
-            type: String
+            type: String,
         },
         line2: {
             type: String,
         },
         city: {
             type: String,
-            required: true
+            require: true
         },
         county: {
             type: String,
@@ -41,28 +93,64 @@ let primaryDataSchema = new Schema({
             type: String,
         }
     }
+	emergency_contact: {
+		contactname: {
+			type: String,
+		},
+		contactnumber: {
+			type: String,
+		},
+		relation: {
+			type: String,
+		}
+	}
+	healthinfo: {
+		height: {
+			type: String,
+		},
+		weight: {
+			type: number(3)
+		},
+		blood_type: {
+			type: String,
+		}
+		pre_conditions: {
+			type: String,
+		}
+		current_meds: {
+			type: String,
+		}
+		allergy: {
+			type: String,
+		}
+	},
+	organization_info: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'organization'
+	}], //Wanted to create a "virtual foreign key" to link data from organization schema, found a relevant example on Stack Overflow here --> https://stackoverflow.com/questions/41175771/mongodbnodejs-foreign-keys-entire-document
 }, {
-    collection: 'primaryData',
+    collection: 'clientData',
     timestamps: true
 });
 
 //collection for eventData
-let eventDataSchema = new Schema({
-    _id: { type: String, default: uuid.v1 },
+let event_data = new Schema({
+    event_id: { type: String, default: uuid.v1 },
+	org_id: {type: String, default: uuid.v1},
     eventName: {
         type: String,
         require: true
     },
     services: {
-        type: Array
+        type: Array,
     },
     date: {
         type: Date,
-        required: true
+        require: true
     },
     address: {
         line1: {
-            type: String
+            type: String,
         },
         line2: {
             type: String,
@@ -81,15 +169,20 @@ let eventDataSchema = new Schema({
         type: String,
     },
     attendees: [{
-        type: String
+        type: String,
     }]
+	organization_info: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'organization'
+	}],
 }, {
     collection: 'eventData'
 });
 
-// create models from mongoose schemas
-const primarydata = mongoose.model('primaryData', primaryDataSchema);
-const eventdata = mongoose.model('eventData', eventDataSchema);
 
+// create models from mongoose schemas
+const organization = mongoose.model('organizationData', org_data);
+const clientinfo = mongoose.model('clientData', client_data);
+const eventdata = mongoose.model('eventData', event_data);
 // package the models in an object to export 
-module.exports = { primarydata, eventdata }
+module.exports = { organization, clientinfo, eventdata }

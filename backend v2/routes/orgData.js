d79d2a296b05908f67325d771e11bcc65ca916ef
faquinn <faquinn@cougarnet.uh.edu>
2022-10-19 16:24:35 -0500

    delete backend v2

--- backend v2/routes/orgData.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"); 
-const router = express.Router(); 
-
-//importing data model schemas
-let { primarydata } = require("../models/models"); 
-let { eventdata } = require("../models/models"); 
-
-//
